93ae01afb7fa80416f902b52788006750e9bfb56
Luke Hoggett <luke.hoggett@gmail.com>
2015-11-22 17:52:23 +1100

    Updated main index to remove ief, added debug to noble script

--- app/main/index.es6.js
+++ app/main/index.es6.js
@@ -1,66 +1,65 @@
-(function () {
-    /* jshint esnext: true */
-    let app = require('app');
-    const ipcMain = require('electron').ipcMain;
-    let crashReporter = require('crash-reporter');
-    let BrowserWindow = require('browser-window');
-    let noble = require('noble');
+/* jshint esnext: true */
+let electron = require('electron');
+let app = electron.app;
+const ipcMain = require('electron').ipcMain;
+let crashReporter = require('crash-reporter');
+let BrowserWindow = require('browser-window');
+let noble = require('noble');
 
-    crashReporter.start({
-      productName: 'es6-ng-electron',
-      companyName: 'FooBar',
-      submitURL: 'http://localhost:3000/',
-      autoSubmit: true
+crashReporter.start({
+  productName: 'es6-ng-electron',
+  companyName: 'FooBar',
+  submitURL: 'http://localhost:3000/',
+  autoSubmit: true
+});
+
+var mainWindow = null;
+
+ipcMain.on('crash', (event, arg) => {
+    console.log("Crash", arg);
+    process.crash(arg);
+});
+
+ipcMain.on('devTools', (event, arg) => {
+    console.log("DevTools", arg);
+    mainWindow.openDevTools();
+});
+
+app.on('window-all-closed', () => {
+    // force app termination on OSX when mainWindow has been closed
+    if (process.platform == 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('ready', () => {
+    console.log("App Ready");
+    mainWindow = new BrowserWindow({
+        // width: 1280,
+        // height: 1200,
+        // x: 1280,
+        // y: 100
+        width: 800,
+        height: 600,
+        x: 960,
+        y: 100
     });
-
-    var mainWindow = null;
-
-    ipcMain.on('crash', (event, arg) => {
-        console.log("Crash", arg);
-        process.crash(arg);
-    });
-
-    ipcMain.on('devTools', (event, arg) => {
-        console.log("DevTools", arg);
-        mainWindow.openDevTools();
-    });
-
-    app.on('window-all-closed', () => {
-        // force app termination on OSX when mainWindow has been closed
-        if (process.platform == 'darwin') {
-            app.quit();
-        }
-    });
-
-    app.on('ready', () => {
-        console.log("App Ready");
-        mainWindow = new BrowserWindow({
-            // width: 1280,
-            // height: 1200,
-            // x: 1280,
-            // y: 100
-            width: 800,
-            height: 600,
-            x: 960,
-            y: 100
+    
+    mainWindow.loadURL('file://' + __dirname + '/../browser/index.html');
+    mainWindow.webContents.on('did-finish-load', () => {
+        console.log("Window Did Load");
+        mainWindow.setTitle(app.getName());
+        
+        console.log("Setting noble state change listener");
+        noble.on('stateChange', (state) => {
+            console.log("Noble State Change", state);
+            noble.startScanning();
+            
         });
         
-        mainWindow.loadURL('file://' + __dirname + '/../browser/index.html');
-        mainWindow.webContents.on('did-finish-load', () => {
-            console.log("Window Did Load");
-            mainWindow.setTitle(app.getName());
-            
-            console.log("Setting noble state change listener");
-            noble.on('stateChange', (state) => {
-                console.log("Noble State Change", state);
-                noble.startScanning();
-                
-            });
-            
-        });
-        mainWindow.on('closed', () => {
-            console.log("Window Closed");
-            mainWindow = null;
-        });
     });
-}());
+    mainWindow.on('closed', () => {
+        console.log("Window Closed");
+        mainWindow = null;
+    });
+});
--- app/noble.js
+++ app/noble.js
@@ -1,5 +1,9 @@
 var noble = require('noble');
 
+var electron = require('electron');
+var app = electron.app;
+ console.debug(app.getPath('exe'));
+ 
 noble.on('stateChange', function(state) {
     console.log("Noble State Change 1", state);
     if (state == 'poweredOn') {
