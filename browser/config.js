97e470f3a613f42b2ee686a03d0a14a7b2e7070a
Luke Hoggett <luke.hoggett@gmail.com>
2016-06-03 09:15:06 +1000

    Updated jspm dependencies

--- browser/config.js
+++ browser/config.js
@@ -15,38 +15,41 @@ System.config({
   },
 
   map: {
-    "angular": "github:angular/bower-angular@1.5.5",
-    "angular-animate": "github:angular/bower-angular-animate@1.5.5",
+    "angular": "github:angular/bower-angular@1.5.6",
+    "angular-animate": "github:angular/bower-angular-animate@1.5.6",
     "angular-localforage": "npm:angular-localforage@1.2.5",
-    "angular-material": "github:angular/bower-material@1.0.8",
+    "angular-material": "github:angular/bower-material@1.0.9",
     "angular-material-icons": "npm:angular-material-icons@0.7.0",
     "angular-order-object-by": "bower:angular-order-object-by@1.3.0",
-    "angular-route": "github:angular/bower-angular-route@1.5.5",
+    "angular-route": "github:angular/bower-angular-route@1.5.6",
     "babel": "npm:babel-core@5.8.38",
     "babel-runtime": "npm:babel-runtime@5.8.38",
     "core-js": "npm:core-js@1.2.6",
-    "mozilla/localForage": "github:mozilla/localForage@1.4.0",
+    "mozilla/localForage": "github:mozilla/localForage@1.4.2",
     "ocombe/angular-localForage": "github:ocombe/angular-localForage@1.2.5",
     "bower:angular-order-object-by@1.3.0": {
-      "angular": "bower:angular@1.5.5"
+      "angular": "bower:angular@1.5.6"
     },
-    "github:angular/bower-angular-animate@1.5.5": {
-      "angular": "github:angular/bower-angular@1.5.5"
+    "github:angular/bower-angular-animate@1.5.6": {
+      "angular": "github:angular/bower-angular@1.5.6"
     },
-    "github:angular/bower-angular-aria@1.5.5": {
-      "angular": "github:angular/bower-angular@1.5.5"
+    "github:angular/bower-angular-aria@1.5.6": {
+      "angular": "github:angular/bower-angular@1.5.6"
     },
-    "github:angular/bower-angular-route@1.5.5": {
-      "angular": "github:angular/bower-angular@1.5.5"
+    "github:angular/bower-angular-route@1.5.6": {
+      "angular": "github:angular/bower-angular@1.5.6"
     },
-    "github:angular/bower-material@1.0.8": {
-      "angular": "github:angular/bower-angular@1.5.5",
-      "angular-animate": "github:angular/bower-angular-animate@1.5.5",
-      "angular-aria": "github:angular/bower-angular-aria@1.5.5",
-      "css": "github:systemjs/plugin-css@0.1.21"
+    "github:angular/bower-material@1.0.9": {
+      "angular": "github:angular/bower-angular@1.5.6",
+      "angular-animate": "github:angular/bower-angular-animate@1.5.6",
+      "angular-aria": "github:angular/bower-angular-aria@1.5.6",
+      "css": "github:systemjs/plugin-css@0.1.22"
     },
     "github:jspm/nodelibs-assert@0.1.0": {
-      "assert": "npm:assert@1.3.0"
+      "assert": "npm:assert@1.4.1"
+    },
+    "github:jspm/nodelibs-buffer@0.1.0": {
+      "buffer": "npm:buffer@3.6.0"
     },
     "github:jspm/nodelibs-path@0.1.0": {
       "path-browserify": "npm:path-browserify@0.0.0"
@@ -66,15 +69,26 @@ System.config({
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
     "npm:angular-material-icons@0.7.0": {
-      "angular": "npm:angular@1.5.5"
+      "angular": "npm:angular@1.5.6"
     },
     "npm:asap@1.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:assert@1.3.0": {
+    "npm:assert@1.4.1": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0",
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.2",
       "util": "npm:util@0.10.3"
     },
     "npm:babel-runtime@5.8.38": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:buffer@3.6.0": {
+      "base64-js": "npm:base64-js@0.0.8",
+      "child_process": "github:jspm/nodelibs-child_process@0.1.0",
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "ieee754": "npm:ieee754@1.1.6",
+      "isarray": "npm:isarray@1.0.0",
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:core-js@1.2.6": {
