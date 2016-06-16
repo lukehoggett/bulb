f3a4ee5c928b50bf63123e61a33fb4640d1f7619
Luke Hoggett <luke.hoggett@gmail.com>
2016-06-16 17:17:22 +1000

    Whitespace

--- main/device-storage.js
+++ main/device-storage.js
@@ -3,74 +3,73 @@
 
 
 
-const storage = require("node-persist");
-let bunyan = require("bunyan");
+  const storage = require("node-persist");
+  let bunyan = require("bunyan");
 
-let log = bunyan.createLogger({
-  name: "bulb"
-});
-
-// initialise storage
-storage.initSync({
-  dir: __dirname + "/../data/device-storage",
-  stringify: JSON.stringify,
-  parse: JSON.parse,
-  encoding: "utf8",
-  logging: false,
-  continuous: true,
-  interval: false,
-  ttl: false
-});
-
-let devices = {};
-
-
-// load all currently stored devices
-let getAll = function () {
-  // @TODO implement caching in local variable, or not as needed
-  
-  let haveCached = Object.keys(devices).length !== 0 && devices.constructor === Object;
-  if (!haveCached) {
-    log.info("device-storage getAll - getting devices from storage");
-    let deviceKeys = storage.keys();
-    log.info("getAll device keys", deviceKeys);
-    deviceKeys.forEach((uuid, index) => {
-      let device = storage.getItem(uuid, (error, device) => {
-        if (error) {
-          log.error(error);
-        }
-      });
-
-      log.info("device-storage loaded UUID:", device.uuid);
-
-      device.stored = true;
-      device.power = false;
-      devices[uuid] = device;
-    });
-  }
-  // log.info("device-storage getAll:", devices);
-  return devices;
-};
-
-let getByUUID = function(uuid) {
-  let devices = getAll();
-  // @TODO do existnce check on uuid first
-  return devices[uuid];
-};
-
-let set = function(uuid, serializedDevice) {
-  
-  storage.setItem(uuid, serializedDevice, error => {
-    if (error) {
-      log.error("Storage error: on set", error);
-    }
+  let log = bunyan.createLogger({
+    name: "bulb"
   });
 
-};
+  // initialise storage
+  storage.initSync({
+    dir: __dirname + "/../data/device-storage",
+    stringify: JSON.stringify,
+    parse: JSON.parse,
+    encoding: "utf8",
+    logging: false,
+    continuous: true,
+    interval: false,
+    ttl: false
+  });
 
-exports.storage = storage;
-exports.getAll = getAll;
-exports.getByUUID = getByUUID;
-exports.set = set;
+  let devices = {};
+
+
+  // load all currently stored devices
+  let getAll = function () {
+    // @TODO implement caching in local variable, or not as needed
+    log.info("device-storage: getAll");
+    let haveCached = Object.keys(devices).length !== 0 && devices.constructor === Object;
+    if (!haveCached) {
+      
+      let deviceKeys = storage.keys();
+      deviceKeys.forEach((uuid, index) => {
+        let device = storage.getItem(uuid, (error, device) => {
+          if (error) {
+            log.error(error);
+          }
+        });
+
+        log.info("device-storage: loaded UUID", device.uuid);
+
+        device.stored = true;
+        device.power = false;
+        devices[uuid] = device;
+      });
+    }
+    return devices;
+  };
+
+  let getByUUID = function(uuid) {
+    let devices = getAll();
+    if (!Object.keys(devices).includes(uuid)) {
+      log.error(`Device ${uuid} is not in storage.`);
+      // @TODO handle error
+    }
+    return devices[uuid];
+  };
+
+  let set = function(uuid, serializedDevice) {
+    storage.setItem(uuid, serializedDevice, error => {
+      if (error) {
+        log.error("Storage error: on set", error);
+      }
+    });
+  };
+
+  exports.storage = storage;
+  exports.getAll = getAll;
+  exports.getByUUID = getByUUID;
+  exports.set = set;
 
 })();
