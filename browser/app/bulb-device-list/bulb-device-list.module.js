03488ff951966827aad354eeed3c3f8cf239a011
Luke Hoggett <luke.hoggett@gmail.com>
2016-08-05 08:32:55 +1000

    WIP component routing

--- browser/app/bulb-device-list/bulb-device-list.module.js
+++ browser/app/bulb-device-list/bulb-device-list.module.js
@@ -1,8 +1,10 @@
 'use strict';
 import { bulbDeviceListComponent } from './bulb-device-list.component';
+import { bulbDeviceListRoute } from './bulb-device-list.route';
 
 export function bulbDeviceList() {
   return angular
     .module('bulb.device-list', [])
-    .directive('bulbDeviceList', bulbDeviceListComponent);
+    .directive('bulbDeviceList', bulbDeviceListComponent)
+    .config(bulbDeviceListRoute);;
 }
